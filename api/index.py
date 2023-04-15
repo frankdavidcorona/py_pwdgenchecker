fa313f9a79f2d3c19fa52690793a9e9ae3ad2ede
Frank Corona Prendes <frank.corona@pm.me>
2023-04-15 14:46:04 -0300

    feat(api): add password generation and strength endpoints This commit adds two new endpoints to the API. The first endpoint generates a password based on the provided parameters, including length, whether to include uppercase letters, digits, and special characters. The second endpoint calculates the strength of a password based on the number of character sets it contains (lowercase letters, uppercase letters, digits, and special characters). These endpoints improve the functionality of the API by providing additional features that can be used by clients.

--- api/index.py
+++ api/index.py
@@ -1,11 +1,61 @@
-from flask import Flask
+import random
+import string
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+def generate_password(length, include_uppercase, include_digits, include_special_chars):
+    character_set = string.ascii_lowercase
+    if include_uppercase:
+        character_set += string.ascii_uppercase
+    if include_digits:
+        character_set += string.digits
+    if include_special_chars:
+        character_set += string.punctuation
+
+    password = ''.join(random.choice(character_set) for _ in range(length))
+    return password
+
+
+def password_strength(password):
+    has_lowercase = any(c in string.ascii_lowercase for c in password)
+    has_uppercase = any(c in string.ascii_uppercase for c in password)
+    has_digit = any(c in string.digits for c in password)
+    has_special_char = any(c in string.punctuation for c in password)
+
+    strength = 0
+    if has_lowercase:
+        strength += 1
+    if has_uppercase:
+        strength += 1
+    if has_digit:
+        strength += 1
+    if has_special_char:
+        strength += 1
+
+    return strength
+
+
+@app.route('/generate_password', methods=['POST'])
+def generate_password_endpoint():
+    length = int(request.json.get('length', 12))
+    include_uppercase = request.json.get('include_uppercase', True)
+    include_digits = request.json.get('include_digits', True)
+    include_special_chars = request.json.get('include_special_chars', True)
+
+    password = generate_password(
+        length, include_uppercase, include_digits, include_special_chars)
+    return jsonify({'password': password})
+
+
+@app.route('/password_strength', methods=['POST'])
+def password_strength_endpoint():
+    password = request.json.get('password', '')
+
+    strength = password_strength(password)
+    return jsonify({'strength': strength})
+
+
+if __name__ == '__main__':
+    app.run()
